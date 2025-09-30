e69472ee8646b689545343ec519ecf463dafcb34
Jose-ant-1 <jgonsol1510@g.educaand.es>
2025-09-30 11:50:58 +0200

    implementacion de cambios

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 console.log('Happy developing ✨');
 
-function controlaCargaDOM() {
+document.addEventListener("DOMContentLoaded", () => {
     console.log("La página ha cargado");
-}
 
-document.addEventListener("DOMContentLoaded", controlaCargaDOM);
+    let PromptOInput = Number(prompt("introduzca 1 para hacer por prompt o 0 para hacer por input"));
+    if (PromptOInput) {
+        pedirNotas();
+        mostrarNotasPorConsola();
+        mostrarNotas();
+        hacerMedia();
+        mostrarMediaPorConsola();
+        mostrarResultado();
+    } else {
+        pedirNotaPorInput();
+    }
+
+});
 
 const mostrar = document.getElementById('alumno');
 
@@ -14,24 +25,73 @@ let nota3;
 let nota4;
 let media;
 
+function pedirNotaPorInput() {
+    mostrar.innerHTML = `<label>Nota 1<input placeholder="Introduzca la nota" type="number" id="nota1" name="nota1" min="0" max="10" required/></label><br>
+                         <label>Nota 2<input placeholder="Introduzca la nota" type="number" id="nota2" name="nota2" min="0" max="10" required/></label><br>
+                         <label>Nota 3<input placeholder="Introduzca la nota" type="number" id="nota3" name="nota3" min="0" max="10" required/></label><br>
+                         <label>Nota 4<input placeholder="Introduzca la nota" type="number" id="nota4" name="nota4" min="0" max="10" required/></label><br>
+                         <button onclick="hacerMediaConInput()">Calcular Media</button>`;
+}
+
+function hacerMediaConInput() {
+    nota1 = Number(document.getElementById('nota1').value);
+    nota2 = Number(document.getElementById('nota2').value);
+    nota3 = Number(document.getElementById('nota3').value);
+    nota4 = Number(document.getElementById('nota4').value);
+    if (!comprobarNumero(nota1)) {
+        mostrar.innerHTML = "Error en la nota 1: Se debe introducir un número entre 0 y 10";
+        return;
+    }
+    if (!comprobarNumero(nota2)) {
+        mostrar.innerHTML = "Error en la nota 2: Se debe introducir un número entre 0 y 10";
+        return;
+    }
+    if (!comprobarNumero(nota3)) {
+        mostrar.innerHTML = "Error en la nota 3: Se debe introducir un número entre 0 y 10";
+        return;
+    }
+    if (!comprobarNumero(nota4)) {
+        mostrar.innerHTML = "Error en la nota 4: Se debe introducir un númeor entre 0 y 10";
+        return;
+    }
+
+    hacerMedia();
+    mostrarNotas();
+    mostrarNotasPorConsola();
+    mostrarMediaPorConsola();
+    mostrarResultado();
+}
+
+
+function mostrarNotas() {
+    mostrar.innerHTML = `Las notas introducidas son: <br>
+                         nota 1 = ${nota1}<br>
+                         nota 2 = ${nota2}<br>
+                         nota 3 = ${nota3}<br>
+                         nota 4 = ${nota4}<br>`;
+}
+
+function mostrarMediaPorConsola() {
+    console.log(`La nota media es ${media}`)
+}
 
 function pedirNotas() {
-    while (isNaN(nota1) || comprobarRangoNumero(nota1)) {
+    while (comprobarNumero(nota1)) {
         nota1 = Number(prompt("inserte la primera nota (entre 0 y 10, vacío es 0)"));
     }
-    while (isNaN(nota2) || comprobarRangoNumero(nota2)) {
+    while (comprobarNumero(nota2)) {
         nota2 = Number(prompt("inserte la segunda nota (entre 0 y 10, vacío es 0)"));
     }
-    while (isNaN(nota3) || comprobarRangoNumero(nota3)) {
+    while (comprobarNumero(nota3)) {
         nota3 = Number(prompt("inserte la tercera nota (entre 0 y 10, vacío es 0)"));
     }
-    while (isNaN(nota4) || comprobarRangoNumero(nota4)) {
+    while (comprobarNumero(nota4)) {
         nota4 = Number(prompt("inserte la cuarta nota (entre 0 y 10, vacío es 0)"));
     }
 }
 
-function comprobarRangoNumero(nota) {
-    return nota < 0 || nota > 10;
+function comprobarNumero(nota) {
+    return nota < 0 || nota > 10 || isNaN(nota);
 }
 
 function hacerMedia() {
@@ -41,13 +101,13 @@ function hacerMedia() {
 function mostrarResultado() {
 
     if (media < 5){
-        mostrar.innerHTML = `El alumno suspendió con media de ${media}`;
+        mostrar.innerHTML += `El alumno suspendió con media de ${media}`;
     } else if (media < 7) {
-        mostrar.innerHTML = `El alumno aprobó con una nota suficiente: ${media}`;
+        mostrar.innerHTML += `El alumno aprobó con una nota suficiente: ${media}`;
     } else if (media < 9) {
-        mostrar.innerHTML = `El alumno aprobó con una nota buena: ${media}`;
+        mostrar.innerHTML += `El alumno aprobó con una nota buena: ${media}`;
     } else {
-        mostrar.innerHTML = `El alumno aprobó con un sobresaliente: ${media}`;
+        mostrar.innerHTML += `El alumno aprobó con un sobresaliente: ${media}`;
     }
 }
 
@@ -56,10 +116,4 @@ function mostrarNotasPorConsola() {
     console.log(`La segunda nota es ${nota2}`);
     console.log(`La tercera nota es ${nota3}`);
     console.log(`La cuarta nota es ${nota4}`);
-    console.log(`La nota media es ${media}`)
 }
-
-pedirNotas();
-hacerMedia();
-mostrarNotasPorConsola();
-mostrarResultado();
